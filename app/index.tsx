cd6ef651f2c294cff839fd911dda6807b3cbb0f2
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-17 17:26:55 -0400

    added nativewind to project

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-blue-500">Hello</Text>
     </View>
   );
 }
