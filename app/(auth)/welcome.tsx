ba3ff4dc55b911d917eafa10c21784243c471ca1
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-18 23:04:09 -0400

    updated welcome page

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const Welcome = () => {
+const Onboarding = () => {
   return (
     <SafeAreaView>
-      <Text>Welcome</Text>
+      <Text>Onboarding</Text>
     </SafeAreaView>
   );
 };
 
-export default Welcome;
+export default Onboarding;
