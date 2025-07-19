503086c8ab215833b186926e2f1a295baffe19af
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-18 23:02:32 -0400

    updated project structure

---
+++ app/(auth)/welcome.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Welcome = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Welcome;
