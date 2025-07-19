ee147f600b5b59d7af318f1697e0a87a6c3967d4
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-18 23:15:11 -0400

    updated index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,15 @@
-import { Text, View } from "react-native";
 import "../global.css";
+//import { useAuth } from "@clerk/clerk-expo";
+import { Href, Redirect } from "expo-router";
 
-export default function Page() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text className="text-blue-500">Hello</Text>
-    </View>
-  );
-}
+const Welcome = "/(auth)/welcome" as Href;
+
+const Page = () => {
+  //const { isSignedIn } = useAuth();
+
+  //if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href={Welcome} />;
+};
+
+export default Page;
