353ccbc477d60f164f70fdd9b59d762b92306a7c
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-20 11:53:15 -0400

    updated sign-up screen

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,22 @@
+import { images } from "@/constants";
+
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const SignUp = () => {
   return (
-    <SafeAreaView>
-      <Text>Sign Up</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-0 w-full h-[250px] " />
+
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Create Your Account
+          </Text>
+          <Text>Sign Up</Text>
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
