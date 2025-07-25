ca2b5f1e53b4f3a99a7cfd69f5a44c4f7cd3be57
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-25 17:31:51 -0400

    updated home page

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -5,7 +5,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-general-500">
       <Text>Home</Text>
       <SignOutButton />
     </SafeAreaView>
