6c373c739ef07abe65759953e1c84343ad5b9887
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-07-31 22:05:16 -0400

    updated

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,8 @@ import * as dotenv from "dotenv";
 // Load environment variables from .env file
 dotenv.config();
 
+const googleKey: string = process.env.GOOGLE_MAPS_API_KEY || "";
+
 // Ensure that the GOOGLE_MAPS_API_KEY is set
 if (!process.env.GOOGLE_MAPS_API_KEY) {
   throw new Error("Missing GOOGLE_MAPS_API_KEY in .env file");
@@ -13,26 +15,72 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   slug: "uber-clone", // required
   version: "1.0.0",
   orientation: "portrait",
+  icon: "./assets/images/icon1.png",
+  scheme: "uberclone",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+
+  web: {
+    bundler: "metro",
+    output: "server",
+    favicon: "./assets/images/favicon.png",
+  },
+
+  experiments: {
+    typedRoutes: true,
+  },
+  extra: {
+    router: {
+      origin: "https://uber.com/",
+    },
+    eas: {
+      projectId: "aaa3379f-2976-43bd-aa87-e6ca215026d0",
+    },
+  },
+
   android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon1.png",
+      backgroundColor: "#ffffff",
+    },
+    edgeToEdgeEnabled: true,
+    package: "com.devcrew.uberclone",
     config: {
       googleMaps: {
-        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        apiKey: googleKey,
       },
     },
   },
   ios: {
+    supportsTablet: true,
     config: {
-      googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      googleMapsApiKey: googleKey,
     },
   },
   plugins: [
+    [
+      "expo-router",
+      {
+        origin: "https://uber.com/",
+      },
+    ],
     "expo-build-properties",
     [
       "react-native-maps",
       {
-        androidGoogleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
-        iosGoogleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+        androidGoogleMapsApiKey: googleKey,
+        iosGoogleMapsApiKey: googleKey,
       },
     ],
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
+      },
+    ],
+    "expo-secure-store",
   ],
 });
