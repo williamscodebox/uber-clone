a5a394b1611cd0ac30b0c31b5e289307c9a62284
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-09 15:35:52 -0400

    updated book-ride

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -1,10 +1,11 @@
-import { useUser } from "@clerk/clerk-expo";
-// import { StripeProvider } from "@stripe/stripe-react-native";
 import Payment from "@/components/Payment";
 import RideLayout from "@/components/RideLayout";
 import { icons } from "@/constants";
 import { formatTime } from "@/lib/utils";
 import { useDriverStore, useLocationStore } from "@/store";
+import { useUser } from "@clerk/clerk-expo";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import { useEffect, useState } from "react";
 import { Image, Text, View } from "react-native";
 
 const BookRide = () => {
@@ -12,97 +13,108 @@ const BookRide = () => {
   const { userAddress, destinationAddress } = useLocationStore();
   const { drivers, selectedDriver } = useDriverStore();
 
+  const [publishableKey, setPublishableKey] = useState("");
+
+  const fetchPublishableKey = async () => {
+    // const key = await fetchKey(); // fetch key from your server here
+    //setPublishableKey(key);
+  };
+
+  useEffect(() => {
+    fetchPublishableKey();
+  }, []);
+
   const driverDetails = drivers?.filter(
     (driver) => +driver.id === selectedDriver
   )[0];
 
   return (
-    // <StripeProvider
-    //   publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
-    //   merchantIdentifier="merchant.com.uber"
-    //   urlScheme="myapp"
-    // >
-    <RideLayout title="Book Ride">
-      <>
-        <Text className="text-xl font-JakartaSemiBold mb-3">
-          Ride Information
-        </Text>
+    <StripeProvider
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
+      merchantIdentifier="merchant.com.uber"
+      urlScheme="myapp"
+    >
+      <RideLayout title="Book Ride">
+        <>
+          <Text className="text-xl font-JakartaSemiBold mb-3">
+            Ride Information
+          </Text>
 
-        <View className="flex flex-col w-full items-center justify-center mt-10">
-          <Image
-            source={{ uri: driverDetails?.profile_image_url }}
-            className="w-28 h-28 rounded-full"
-          />
+          <View className="flex flex-col w-full items-center justify-center mt-10">
+            <Image
+              source={{ uri: driverDetails?.profile_image_url }}
+              className="w-28 h-28 rounded-full"
+            />
 
-          <View className="flex flex-row items-center justify-center mt-5 space-x-2">
-            <Text className="text-lg font-JakartaSemiBold">
-              {driverDetails?.title}
-            </Text>
+            <View className="flex flex-row items-center justify-center mt-5 space-x-2">
+              <Text className="text-lg font-JakartaSemiBold">
+                {driverDetails?.title}
+              </Text>
 
-            <View className="flex flex-row items-center space-x-0.5">
-              <Image
-                source={icons.star}
-                className="w-5 h-5"
-                resizeMode="contain"
-              />
+              <View className="flex flex-row items-center space-x-0.5">
+                <Image
+                  source={icons.star}
+                  className="w-5 h-5"
+                  resizeMode="contain"
+                />
+                <Text className="text-lg font-JakartaRegular">
+                  {driverDetails?.rating}
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View className="flex flex-col w-full items-start justify-center py-3 px-5 rounded-3xl bg-general-600 mt-5">
+            <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
+              <Text className="text-lg font-JakartaRegular">Ride Price</Text>
+              <Text className="text-lg font-JakartaRegular text-[#0CC25F]">
+                {/* ${driverDetails?.price}  ///// use this code once data stream is completed in app setup */}
+                ${driverDetails?.price || 50!}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
+              <Text className="text-lg font-JakartaRegular">Pickup Time</Text>
               <Text className="text-lg font-JakartaRegular">
-                {driverDetails?.rating}
+                {/* {formatTime(driverDetails?.time!)} ///// use this code once data stream is completed in app setup */}
+                {formatTime(driverDetails?.time || 5!)}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-between w-full py-3">
+              <Text className="text-lg font-JakartaRegular">Car Seats</Text>
+              <Text className="text-lg font-JakartaRegular">
+                {driverDetails?.car_seats}
               </Text>
             </View>
           </View>
-        </View>
 
-        <View className="flex flex-col w-full items-start justify-center py-3 px-5 rounded-3xl bg-general-600 mt-5">
-          <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
-            <Text className="text-lg font-JakartaRegular">Ride Price</Text>
-            <Text className="text-lg font-JakartaRegular text-[#0CC25F]">
-              {/* ${driverDetails?.price}  ///// use this code once data strrem is completed in app setup */}
-              ${driverDetails?.price || 50!}
-            </Text>
+          <View className="flex flex-col w-full items-start justify-center mt-5">
+            <View className="flex flex-row items-center justify-start mt-3 border-t border-b border-general-700 w-full py-3">
+              <Image source={icons.to} className="w-6 h-6" />
+              <Text className="text-lg font-JakartaRegular ml-2">
+                {userAddress}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-start border-b border-general-700 w-full py-3">
+              <Image source={icons.point} className="w-6 h-6" />
+              <Text className="text-lg font-JakartaRegular ml-2">
+                {destinationAddress}
+              </Text>
+            </View>
           </View>
 
-          <View className="flex flex-row items-center justify-between w-full border-b border-white py-3">
-            <Text className="text-lg font-JakartaRegular">Pickup Time</Text>
-            <Text className="text-lg font-JakartaRegular">
-              {/* {formatTime(driverDetails?.time!)} ///// use this code once data strrem is completed in app setup */}
-              {formatTime(driverDetails?.time || 5!)}
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center justify-between w-full py-3">
-            <Text className="text-lg font-JakartaRegular">Car Seats</Text>
-            <Text className="text-lg font-JakartaRegular">
-              {driverDetails?.car_seats}
-            </Text>
-          </View>
-        </View>
-
-        <View className="flex flex-col w-full items-start justify-center mt-5">
-          <View className="flex flex-row items-center justify-start mt-3 border-t border-b border-general-700 w-full py-3">
-            <Image source={icons.to} className="w-6 h-6" />
-            <Text className="text-lg font-JakartaRegular ml-2">
-              {userAddress}
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center justify-start border-b border-general-700 w-full py-3">
-            <Image source={icons.point} className="w-6 h-6" />
-            <Text className="text-lg font-JakartaRegular ml-2">
-              {destinationAddress}
-            </Text>
-          </View>
-        </View>
-
-        <Payment
-          fullName={user?.fullName!}
-          email={user?.emailAddresses[0].emailAddress!}
-          amount={driverDetails?.price!}
-          driverId={Number(driverDetails?.id)}
-          rideTime={driverDetails?.time!}
-        />
-      </>
-    </RideLayout>
-    // {/* </StripeProvider> */}
+          <Payment
+            fullName={user?.fullName!}
+            email={user?.emailAddresses[0].emailAddress!}
+            amount={driverDetails?.price!}
+            driverId={Number(driverDetails?.id)}
+            rideTime={driverDetails?.time!}
+          />
+        </>
+      </RideLayout>
+    </StripeProvider>
   );
 };
 
--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,12 +1,13 @@
 import { useAuth } from "@clerk/clerk-expo";
-// import { useStripe } from "@stripe/stripe-react-native";
+import { useStripe } from "@stripe/stripe-react-native";
 import { router } from "expo-router";
 import React, { useState } from "react";
-import { Image, Text, View } from "react-native";
+import { Alert, Image, Text, View } from "react-native";
 import { ReactNativeModal } from "react-native-modal";
 
 import CustomButton from "@/components/CustomButton";
 import { images } from "@/constants";
+import { fetchAPI } from "@/lib/fetch";
 import { useLocationStore } from "@/store";
 import { PaymentProps } from "@/types/type";
 
@@ -17,7 +18,7 @@ const Payment = ({
   driverId,
   rideTime,
 }: PaymentProps) => {
-  // const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
   const {
     userAddress,
     userLongitude,
@@ -30,96 +31,96 @@ const Payment = ({
   const { userId } = useAuth();
   const [success, setSuccess] = useState<boolean>(false);
 
-  //   const openPaymentSheet = async () => {
-  //     await initializePaymentSheet();
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
 
-  //     const { error } = await presentPaymentSheet();
+    const { error } = await presentPaymentSheet();
 
-  //     if (error) {
-  //       Alert.alert(`Error code: ${error.code}`, error.message);
-  //     } else {
-  //       setSuccess(true);
-  //     }
-  //   };
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+  };
 
-  //   const initializePaymentSheet = async () => {
-  // const { error } = await initPaymentSheet({
-  //   merchantDisplayName: "Example, Inc.",
-  //   intentConfiguration: {
-  //     mode: {
-  //       amount: parseInt(amount) * 100,
-  //       currencyCode: "usd",
-  //     },
-  //     confirmHandler: async (
-  //       paymentMethod,
-  //       shouldSavePaymentMethod,
-  //       intentCreationCallback,
-  //     ) => {
-  //           const { paymentIntent, customer } = await fetchAPI(
-  //             "/(api)/(stripe)/create",
-  //             {
-  //               method: "POST",
-  //               headers: {
-  //                 "Content-Type": "application/json",
-  //               },
-  //               body: JSON.stringify({
-  //                 name: fullName || email.split("@")[0],
-  //                 email: email,
-  //                 amount: amount,
-  //                 paymentMethodId: paymentMethod.id,
-  //               }),
-  //             },
-  //           );
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: parseInt(amount) * 100,
+          currencyCode: "usd",
+        },
+        confirmHandler: async (
+          paymentMethod,
+          shouldSavePaymentMethod,
+          intentCreationCallback
+        ) => {
+          const { paymentIntent, customer } = await fetchAPI(
+            "/(api)/(stripe)/create",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                name: fullName || email.split("@")[0],
+                email: email,
+                amount: amount,
+                paymentMethodId: paymentMethod.id,
+              }),
+            }
+          );
 
-  //           if (paymentIntent.client_secret) {
-  //             const { result } = await fetchAPI("/(api)/(stripe)/pay", {
-  //               method: "POST",
-  //               headers: {
-  //                 "Content-Type": "application/json",
-  //               },
-  //               body: JSON.stringify({
-  //                 payment_method_id: paymentMethod.id,
-  //                 payment_intent_id: paymentIntent.id,
-  //                 customer_id: customer,
-  //                 client_secret: paymentIntent.client_secret,
-  //               }),
-  //             });
+          if (paymentIntent.client_secret) {
+            const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                payment_method_id: paymentMethod.id,
+                payment_intent_id: paymentIntent.id,
+                customer_id: customer,
+                client_secret: paymentIntent.client_secret,
+              }),
+            });
 
-  //             if (result.client_secret) {
-  //               await fetchAPI("/(api)/ride/create", {
-  //                 method: "POST",
-  //                 headers: {
-  //                   "Content-Type": "application/json",
-  //                 },
-  //                 body: JSON.stringify({
-  //                   origin_address: userAddress,
-  //                   destination_address: destinationAddress,
-  //                   origin_latitude: userLatitude,
-  //                   origin_longitude: userLongitude,
-  //                   destination_latitude: destinationLatitude,
-  //                   destination_longitude: destinationLongitude,
-  //                   ride_time: rideTime.toFixed(0),
-  //                   fare_price: parseInt(amount) * 100,
-  //                   payment_status: "paid",
-  //                   driver_id: driverId,
-  //                   user_id: userId,
-  //                 }),
-  //               });
+            if (result.client_secret) {
+              await fetchAPI("/(api)/ride/create", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  origin_address: userAddress,
+                  destination_address: destinationAddress,
+                  origin_latitude: userLatitude,
+                  origin_longitude: userLongitude,
+                  destination_latitude: destinationLatitude,
+                  destination_longitude: destinationLongitude,
+                  ride_time: rideTime.toFixed(0),
+                  fare_price: parseInt(amount) * 100,
+                  payment_status: "paid",
+                  driver_id: driverId,
+                  user_id: userId,
+                }),
+              });
 
-  //               intentCreationCallback({
-  //                 clientSecret: result.client_secret,
-  //               });
-  //             }
-  //           }
-  //         },
-  //       },
-  //       returnURL: "myapp://book-ride",
-  //     });
+              intentCreationCallback({
+                clientSecret: result.client_secret,
+              });
+            }
+          }
+        },
+      },
+      returnURL: "myapp://book-ride",
+    });
 
-  //     if (!error) {
-  //       // setLoading(true);
-  //     }
-  //   };
+    if (!error) {
+      // setLoading(true);
+    }
+  };
 
   return (
     <>
