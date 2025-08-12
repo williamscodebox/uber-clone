4d964b9d731fbab478a3f9953331115460c5f801
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-08-12 12:17:02 -0400

    updated map

--- lib/map.ts
+++ lib/map.ts
@@ -94,23 +94,23 @@ export const calculateDriverTimes = async ({
     return;
 
   try {
-    const timesPromises = markers.map(async (marker) => {
-      const responseToUser = await fetch(
-        `https://maps.googleapis.com/maps/api/directions/json?origin=${marker.latitude},${marker.longitude}&destination=${userLatitude},${userLongitude}&key=${directionsAPI}`
-      );
-      const dataToUser = await responseToUser.json();
-      const timeToUser = dataToUser.routes[0].legs[0].duration.value; // Time in seconds
-      const responseToDestination = await fetch(
-        `https://maps.googleapis.com/maps/api/directions/json?origin=${userLatitude},${userLongitude}&destination=${destinationLatitude},${destinationLongitude}&key=${directionsAPI}`
-      );
-      const dataToDestination = await responseToDestination.json();
-      const timeToDestination =
-        dataToDestination.routes[0].legs[0].duration.value; // Time in seconds
-      const totalTime = (timeToUser + timeToDestination) / 60; // Total time in minutes
-      const price = (totalTime * 0.5).toFixed(2); // Calculate price based on time
-      return { ...marker, time: totalTime, price };
-    });
-    return await Promise.all(timesPromises);
+    // const timesPromises = markers.map(async (marker) => {
+    //   const responseToUser = await fetch(
+    //     `https://maps.googleapis.com/maps/api/directions/json?origin=${marker.latitude},${marker.longitude}&destination=${userLatitude},${userLongitude}&key=${directionsAPI}`
+    //   );
+    //   const dataToUser = await responseToUser.json();
+    //   const timeToUser = dataToUser.routes[0].legs[0].duration.value; // Time in seconds
+    //   const responseToDestination = await fetch(
+    //     `https://maps.googleapis.com/maps/api/directions/json?origin=${userLatitude},${userLongitude}&destination=${destinationLatitude},${destinationLongitude}&key=${directionsAPI}`
+    //   );
+    //   const dataToDestination = await responseToDestination.json();
+    //   const timeToDestination =
+    //     dataToDestination.routes[0].legs[0].duration.value; // Time in seconds
+    //   const totalTime = (timeToUser + timeToDestination) / 60; // Total time in minutes
+    //   const price = (totalTime * 0.5).toFixed(2); // Calculate price based on time
+    //   return { ...marker, time: totalTime, price };
+    // });
+    // return await Promise.all(timesPromises);
   } catch (error) {
     console.error("Error calculating driver times:", error);
   }
